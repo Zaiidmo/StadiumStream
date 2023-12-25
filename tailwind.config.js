0679c89f524e807dc41140589f752d4b6579c2ae
Zaiid Moumnii <zaiidmoumnii@gmail.com>
2023-12-25 15:54:23 +0100

    Configuring TailwindCss

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./Public/pages/**/*.{html,js,php}"],
+  content: [
+    "./public/*.php",
+    "./app/View/*.php",
+    "./app/View/includes/*.php",
+    "./lang/*.php",
+    "./lang/*.html",
+    "./js/custom.min.js",
+  ],
+
+  "tailwindCSS.includeLanguages": {
+    plaintext: "php",
+  },
+
+  plugins: [],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+};
