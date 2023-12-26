6ac0f6a01adfc4d94a5c7a0e1f6d973c7dc7e720
khalid oukha <oukhakhalid@gmail.com>
2023-12-26 21:57:04 +0100

    added hero section/match cards

--- app/View/home.php
+++ app/View/home.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,43 +9,220 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
 
 </head>
-<body class="dark:bg-gray-900">
-<?php
-        include '../app/View/includes/navbar.php';
-?>
 
-<section>
-<div class="relative">
-  <img src="public/assets/images/herobg.svg" class="absolute inset-0 object-cover w-full h-full" alt="" />
-  <div class="relative customgradient" >
-    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <div class="flex flex-col items-center justify-between xl:flex-row">
-        <div class="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
-          <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
-          Feel the Passion: Get Your Ticket to the Epic <br class="hidden md:block" />
-            <span class="text-teal-accent-400">African Football Journey!</span>
-          </h2>
-          <p class="max-w-xl mb-4 text-base text-gray-400 md:text-lg">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudan, totam rem aperiam, eaque ipsa quae.
-          </p>
-          <a href="/" aria-label="" class="inline-flex items-center font-semibold tracking-wider transition-colors duration-200 text-teal-accent-400 hover:text-teal-accent-700">
-            Learn more
-            <svg class="inline-block w-3 ml-2" fill="currentColor" viewBox="0 0 12 12">
-              <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z"></path>
-            </svg>
-          </a>
-        </div>
-        <div class="w-full max-w-xl xl:px-8 xl:w-5/12">
-          <img src="public/assets/images/player.svg"
-            class="w-full rounded-lg shadow-lg dark:shadow-black/20" alt="" />
-        </div>
-      </div>
-    </div>
-  </div>
-</div>  
-</section>
+<body class="">
+    <?php
+    include '../app/View/includes/navbar.php';
+    ?>
 
-<!-- Section: Design Block -->
+    <section class="relative">
+        <div class="relative">
+            <img src="public/assets/images/herobg.svg" class="absolute inset-0 object-cover w-full h-full" alt="" />
+            <div class="relative customgradient">
+                <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+                    <div class="flex flex-col items-center justify-between xl:flex-row">
+                        <div class="w-full max-w-3xl mb-10 xl:mb-0  xl:w-12/16">
+                            <h2 class="max-w-3/5 mb-10 font-sans text-7xl font-bold tracking-tight text-white">
+                                Feel the Passion: Get Your Ticket to the Epic African
+                                <br class="hidden md:block" />
+                                <span class=" text-orange text-7xl font-bold ">Football Journey!</span>
+                            </h2>
+
+
+                            <p class="max-w-xl mb-4 text-base text-gray-200 md:text-lg">
+                                Look no further! Our SBS The Show tickets are the simplest way for you to experience a live Kpop recording. </p>
+                            <button type="button" class="text-white bg-orange hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-3.5 text-center inline-flex items-center dark:bg-orange-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                                Get Started
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                                <button type="button" class="mx-5 text-white bg-orange hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-3.5 text-center inline-flex items-center dark:bg-orange-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                                    Get Your Ticket
+                                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                    </svg>
+                        </div>
+                        <div class="w-full max-w-xl xl:px-8 xl:w-5/12 relative">
+                            <img src="public/assets/images/player.svg" class="w-full rounded-lg h-auto" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="bg-gray-50 w-full dark:bg-gray-900 ">
+        <div class=" container max-w-7xl my-5 flex justify-center mx-auto">
+            <!-- Start coding here -->
+            <div class="w-full bg-primary-100  py-8 shadow-md dark:bg-gray-800 sm:rounded-lg">
+                <div class="flex flex-col items-center justify-between p-4 space-y-3 md:flex-row md:space-y-0 md:space-x-4">
+                    <div class="w-full md:w-1/2 py-2">
+                        <label for="phone-input" class="block mb-5 text-s font-medium text-white dark:text-white">Search For Match :</label>
+                        <form class="flex items-center">
+                            <label for="simple-search" class="sr-only">Search Match</label>
+                            <div class="relative w-full">
+                                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                                <input type="text" id="simple-search" class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-green-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search For Match" required="">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="w-full md:w-1/2 py-2">
+                        <label for="phone-input" class="block mb-5 text-s font-medium text-white dark:text-white">Search For stadium :</label>
+
+                        <form class="flex items-center">
+                            <label for="simple-search" class="sr-only">Search</label>
+                            <div class="relative w-full">
+                                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                                <input type="text" id="simple-search" class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search For Stadium" required="">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="w-full md:w-1/3 py-2">
+                        <label for="phone-input" class="block mb-5 text-s font-medium text-white dark:text-white">Filter by Date</label>
+                        <button id="filterDropdownButton" data-dropdown-toggle="filterDropdown" class="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" type="button">
+                            <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="w-4 h-4 mr-2 text-gray-400" viewbox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" />
+                            </svg>
+                            Filter
+                            <svg class="-mr-1 ml-1.5 w-5 h-5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <path clip-rule="evenodd" fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
+                            </svg>
+                        </button>
+                        <!-- Dropdown menu -->
+                        <div id="filterDropdown" class="z-10 hidden w-48 p-3 bg-white rounded-lg shadow dark:bg-gray-700">
+                            <h6 class="mb-3 text-sm font-medium text-gray-900 dark:text-white">
+                                Upcoming Match
+                            </h6>
+                            <ul class="space-y-2 text-sm" aria-labelledby="dropdownDefault">
+                                <li class="flex items-center">
+                                    <input id="apple" type="checkbox" value="" class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                    <label for="apple" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                                        Apple (56)
+                                    </label>
+                                </li>
+                                <li class="flex items-center">
+                                    <input id="fitbit" type="checkbox" value="" class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                    <label for="fitbit" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                                        Fitbit (56)
+                                    </label>
+                                </li>
+                                <li class="flex items-center">
+                                    <input id="dell" type="checkbox" value="" class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                    <label for="dell" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                                        Dell (56)
+                                    </label>
+                                </li>
+                                <li class="flex items-center">
+                                    <input id="asus" type="checkbox" value="" checked class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                    <label for="asus" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                                        Asus (97)
+                                    </label>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="py-4">
+        <div>
+            <h2 class=" text-2xl font-bold text-center sm:text-4xl text-orange">Upcoming Matchs</h2>
+            <p class="max-w-3xl mx-auto mt-4 text-xl text-center ">Explore the latest features that enhance your learning experience and make it even more exciting.</p>
+        </div>
+        <div class="container max-w-7xl px-2 mx-auto">
+            <div class="grid-cols-1 sm:grid md:grid-cols-3 ">
+                <div class="mx-3 mt-6 flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0">
+                    <a href="#!">
+                        <img class="rounded-t-lg" src="public/assets/images/match4.jpg" alt="Skyscrapers" />
+                    </a>
+                    <div class="p-3">
+                        <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                            Card title
+                        </h5>
+                        <p class=" text-base text-neutral-600 dark:text-neutral-200">
+                            This is a wider card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </p>
+                    </div>
+                    <div class="p-3 flex justify-end">
+                        <a href="" class="text-orange border border-orange py-2 px-3 rounded inline-flex  items-center">
+                            Buy Ticket
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6 ml-2">
+                                <path d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="mt-auto border-t-2 border-neutral-100 px-6 py-3 text-center dark:border-neutral-600 dark:text-neutral-50">
+                        <small>Last updated 3 mins ago</small>
+                    </div>
+                </div>
+                <div class="mx-3 mt-6 flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0">
+                    <a href="#!">
+                        <img class="rounded-t-lg" src="public/assets/images/match4.jpg" alt="Skyscrapers" />
+                    </a>
+                    <div class="p-3">
+                        <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                            Card title
+                        </h5>
+                        <p class=" text-base text-neutral-600 dark:text-neutral-200">
+                            This is a wider card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </p>
+                    </div>
+                    <div class="p-3 flex justify-end">
+                        <a href="" class="text-orange border border-orange py-2 px-3 rounded inline-flex  items-center">
+                            Buy Ticket
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6 ml-2">
+                                <path d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="mt-auto border-t-2 border-neutral-100 px-6 py-3 text-center dark:border-neutral-600 dark:text-neutral-50">
+                        <small>Last updated 3 mins ago</small>
+                    </div>
+                </div>
+                <div class="mx-3 mt-6 flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0">
+                    <a href="#!">
+                        <img class="rounded-t-lg" src="public/assets/images/match4.jpg" alt="Skyscrapers" />
+                    </a>
+                    <div class="p-3">
+                        <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                            Card title
+                        </h5>
+                        <p class=" text-base text-neutral-600 dark:text-neutral-200">
+                            This is a wider card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </p>
+                    </div>
+                    <div class="p-3 flex justify-end">
+                        <a href="" class="text-orange border border-orange py-2 px-3 rounded inline-flex  items-center">
+                            Buy Ticket
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6 ml-2">
+                                <path d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="mt-auto border-t-2 border-neutral-100 px-6 py-3 text-center dark:border-neutral-600 dark:text-neutral-50">
+                        <small>Last updated 3 mins ago</small>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
 </body>
 <script src="public/assets/js/theme.js"></script>
+
 </html>
--- app/View/includes/navbar.php
+++ app/View/includes/navbar.php
@@ -1,6 +1,6 @@
 
 
-<nav class=" bg-[#0D4B33] fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+<nav class=" bg-[#0D4B33] fixed w-full z-20 top-0 start-0 dark:border-gray-600">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
       <img src="public/assets/images/logo.png" class="h-8" alt="Flowbite Logo">
