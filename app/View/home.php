0c667f1211ce60399ba5360cc07ed4f91d71f301
khalid oukha <oukhakhalid@gmail.com>
2023-12-26 12:39:51 +0100

    added a navbar

---
+++ app/View/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="public/assets/dist/output.css">
+</head>
+<body class="dark:bg-gray-900">
+    <?php
+        include '../app/View/includes/navbar.php';
+    ?>
+</body>
+<script src="public/assets/js/theme.js"></script>
+</html>
