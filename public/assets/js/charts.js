8881eaf41a3326db2070f9293664dd054c10d0dd
Zaiid Moumnii <zaiidmoumnii@gmail.com>
2023-12-28 00:21:11 +0100

    updating tickets graph

--- public/assets/js/charts.js
+++ public/assets/js/charts.js
@@ -1,15 +1,15 @@
-const xValues = [50,60,70,80,90,100,110,120,130,140,150];
-const yValues = [7,8,8,9,9,9,10,11,14,14,15];
+const xValues = ['Monday',60,70,80,90,100,110,120,130,140,150];
+const yValues = [10,8,5,9,10,17,3,11,7,14,1];
 
 new Chart("myChart", {
   type: "line",
   data: {
     labels: xValues,
     datasets: [{
-      backgroundColor:"rgba(0,75,50,1.0)",
+      // backgroundColor:"rgba(0,75,50,1.0)",
       borderColor: "rgba(255,110,0,0.5)",
       data: yValues
     }]
   },
-  // options:{...}
+  options:{tickets}
 });
