c39840af759ee9794c4b989bb9419066a53f9d58
khalid oukha <oukhakhalid@gmail.com>
2024-01-01 22:50:55 +0100

    real matches data inserted & dynamic content home page

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -1,7 +1,12 @@
 <?php 
 namespace App\Controller;
+
+use App\Model\HomeModel;
+
 class HomeController {
     public function index() {
+        $obj = new HomeModel();
+        $matches = $obj->fetchAllMatches();
         include '../app/View/home.php';
     }
 }
---
+++ app/Model/HomeModel.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Model;
+use config\Connection;
+use PDO;
+use PDOException;
+class HomeModel extends MatchModel{
+    public function fetchAllMatches(){
+       return $this->fetchMatches();
+    }
+}
