58e263807a058fa55e2f239c5d712eb39eea421c
Zaiid Moumnii <zaiidmoumnii@gmail.com>
2024-01-02 10:35:09 +0100

    Dashboard back end done

--- app/View/dashboard/tickets.php
+++ app/View/dashboard/tickets.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Management</title>
+    <title>Tickets Management</title>
     <link rel="stylesheet" href="public/assets/dist/output.css">
 </head>
 
