5ed3827e8a1f4d2a2b6f8d02c5d609cc313a978f
Zaiid Moumnii <zaiidmoumnii@gmail.com>
2024-01-05 09:27:49 +0100

    Adding Carbon Conditions

--- app/Controller/BuyticketController.php
+++ app/Controller/BuyticketController.php
@@ -1,41 +1,53 @@
 <?php
 
 namespace App\Controller;
+
 session_start();
 
 use app\fpdf\TicketPDF;
 use App\Model\buyticketModel;
 use App\Model\MatchModel;
+use Carbon\Carbon;
 
 class BuyticketController extends MatchModel
 {
     protected $userid;
-        public function index($id){
-            $obj = new buyticketModel();
-            $singlematch =  $obj->fetchSingleMatche($id);
-            // var_dump($singlematch);die;
-            // echo $singlematch->team1;
+    public function index($id)
+    {
+        $obj = new buyticketModel();
+        $singlematch =  $obj->fetchSingleMatche($id);
+        // var_dump($singlematch);die;
+        // echo $singlematch->team1;
 
-            require "../app/View/buyticket.php";
-        }
+        require "../app/View/buyticket.php";
+    }
 
-        public function reserveTicket($id){
-            $ticket = new buyticketModel();
-            $userid = $_SESSION['id'];
-            $ticketid = $id . $userid . time();
-            $uu_id = hash('sha1', $ticketid);
-            $data = $ticket->fetchSingleMatche($id);
-            $price = $data['price'];
-            // var_dump($uu_id);die;
+    public function reserveTicket($id)
+    {
+        $ticket = new buyticketModel();
+        $userid = $_SESSION['id'];
+        $ticketid = $id . $userid . time();
+        $uu_id = hash('sha1', $ticketid);
+        $data = $ticket->fetchSingleMatche($id);
+        $price = $data['price'];
+        $gameTime = $data['matchDay'];
+        $currentTime = Carbon::now();
+        $remaining_days = $currentTime->diffInDays($gameTime, false);
+        if ($remaining_days < 1) {
+            $_SESSION['error'] = "You can't reserve a ticket for this match";
+            header("Location: " . URL_DIR . "home");
+            exit();
+        } else {
             $obj = new buyticketModel();
-            $obj->reserveTicket($id,$userid,$uu_id,$price);
+            $obj->reserveTicket($id, $userid, $uu_id, $price);
             $this->generateTicket($id);
         }
+    }
 
     public function generateTicket($id)
     {
         $ticket = new buyticketModel();
-        global $data ;
+        global $data;
         $data = $ticket->fetchSingleMatche($id);
         require '../app/PDF/pdf.php';
         $pdf = new TicketPDF();
